a7fc2b6e73ae573fb453e17058f6ce300bb5a49d
Shubham <bakhalshubham@gmail.com>
2021-06-16 12:56:44 +0530

    added heroku url link

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -23,7 +23,7 @@ const TextEditor = () => {
   const { id: documentId } = useParams();
 
   useEffect(() => {
-    const s = io('http://localhost:3001');
+    const s = io('https://texteditorrealtime.herokuapp.com/');
     setSocket(s);
 
     return () => {
